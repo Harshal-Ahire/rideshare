c4cad217543fbc28e2fd57314f15d4a14fb937af
Harshal <141637336+Harshal-Ahire@users.noreply.github.com>
2026-07-02 13:08:59 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: 'public', //  Tells Vite to look inside the public folder for index.html
+  build: {
+    outDir: '../dist', // Tells Vite to output the final build files in the correct place for Vercel
+    emptyOutDir: true,
+  },
   server: {
     port: 3000,
   },
